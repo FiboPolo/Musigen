154fdbf509c5125f56e70aadf62f51824339c235
Steve PB <spoloboyd@gmail.com>
2023-04-27 18:05:44 +0200

    views set up

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ require("./db");
 // https://www.npmjs.com/package/express
 const express = require("express");
 
+
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
