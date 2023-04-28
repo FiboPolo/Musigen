bd743ed124050970c17f910aeeacdcd92a691776
John Doe <johndoe@example.com>
2023-04-28 14:40:41 +0100

    signup/login routes done. Currently working on sessions/cookies

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,6 +4,8 @@ const User = require("../models/User.model");
 const bcryptjs = require("bcryptjs");
 const saltRounds = 10;
 
+const pwdRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{8,}$/; // spec char, a capital, 8 characters long
+
 router.get("/", (req, res, next) => {});
 
 //GET for sign up
@@ -16,17 +18,27 @@ router.post("/signup", async (req, res, next) => {
   try {
     const potentialUser = await User.findOne({ username: req.body.username });
     if (!potentialUser) {
-      const salt = bcryptjs.genSaltSync(saltRounds);
+      if (pwdRegex.test(req.body.password)) {
+        const salt = bcryptjs.genSaltSync(saltRounds);
 
-      const passwordHash = bcryptjs.hashSync(req.body.password, salt);
+        const passwordHash = bcryptjs.hashSync(req.body.password, salt);
 
-      const newUser = await User.create({
-        username: req.body.username,
-        passwordHash,
-      });
-      res.redirect("/auth/login");
+        const newUser = await User.create({
+          username: req.body.username,
+          passwordHash,
+        });
+        res.redirect("/auth/login");
+      } else {
+        res.render("auth/signup", {
+          errorMessage: "Password not strong enough ",
+          data: { username: req.body.username },
+        });
+      }
     } else {
-      res.render("auth/signup", { errorMessage: "Username already in use" });
+      res.render("auth/signup", {
+        errorMessage: "Username already in use",
+        data: { username: req.body.username },
+      });
     }
   } catch (error) {
     console.log(error);
@@ -39,5 +51,26 @@ router.get("/login", (req, res, next) => {
 });
 
 //POST for log in form
+router.post("/login", async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    console.log(user);
+    if (!!user) {
+      if (bcryptjs.compareSync(req.body.password, user.passwordHash)) {
+        res.render("auth/profile", { user });
+      } else {
+        res.render("auth/login", {
+          errorMessage: "Wrong password",
+        });
+      }
+    } else {
+      res.render("auth/login", {
+        errorMessage: "User does not exist",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
