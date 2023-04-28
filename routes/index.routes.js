2ccae3f944d6aecf39ea5dbc87389a463fe1d024
John Doe <johndoe@example.com>
2023-04-28 15:58:22 +0100

    protected login route secured

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { isLoggedIn } = require("../middleware/route.guard");
 const router = express.Router();
 
 /* GET home page */
@@ -6,7 +7,7 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-router.get("/profile", (req, res, next) => {
+router.get("/profile", isLoggedIn, (req, res, next) => {
   console.log(req.session);
   res.render("profile", { user: req.session.user });
 });
