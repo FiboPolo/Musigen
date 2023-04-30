13d26f9883a74baad598b9e93abd3ef616da1277
John Doe <johndoe@example.com>
2023-04-30 18:36:50 +0100

    musician create post route created

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const Musician = require("../models/musician.model");
 
 /* GET home page */
 router.get("/createMusician", (req, res, next) => {
   res.render("profile/createMusician");
 });
 
+router.post("/createMusician", async (req, res, next) => {
+  const createdMusician = await Musician.create(req.body);
+  console.log("new musician", createdMusician);
+  res.redirect("profile/musicianList");
+});
+
 router.get("/MusicianList", (req, res, next) => {
-  res.render("profile/MusicianList");
+  res.render("profile/musicianList");
 });
 
 module.exports = router;
